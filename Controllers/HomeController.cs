d235c5e0e99d8cca7ad3d8c02da49db164507fe4
Harieswaran D <harieswaran.dhanasekar@epsilon.com>
2022-05-06 17:10:05 +0530

    api added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Application.CardQuery;
+using card_sorting_api1.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace card_sorting_api1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public readonly ICardQuery _cardQuery;
+        public HomeController(ICardQuery cardQuery)
+        {
+            _cardQuery = cardQuery;
+        }
+        [HttpPost]
+        public IEnumerable<Card> GetSortedCards(IEnumerable<Card> cards)
+        {
+            return cards.OrderBy(card=>card.priority);
+
+        }
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+            var cards = _cardQuery.Get();
+            return cards;
+
+        }
+    }
+}
